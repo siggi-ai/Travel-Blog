ecc78097f656d8156a3f0c1ec3a683d7ac17fd23
siggi <siegfried.hamm@gmail.com>
2021-10-05 15:12:27 +0200

    .

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,7 @@ export default {
     };
   },
   mounted: async function () {
-    const url = "http://localhost:8080/mockData.json";
+    const url = "https://kind-davinci-ca9573.netlify.app/mockdata.json";
     const response = await fetch(url);
     const result = await response.json();
     this.apiResponseData = result;
