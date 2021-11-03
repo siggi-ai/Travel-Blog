b23bbe750e7635c0660d23b5916f304b544e6a47
siggi <siegfried.hamm@gmail.com>
2021-11-03 09:37:07 +0100

    about fine

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,7 +7,9 @@
       also designed some of the Logos. Please contact me! <br />
       My assistant Smiley Face will answer all questions.
     </p>
-    <router-link class="navButtonContact" to="/contact">click for contact 👽</router-link>
+    <router-link class="navButtonContact" to="/contact"
+      >click for contact 👽</router-link
+    >
   </div>
 </template>
 
@@ -19,11 +21,43 @@
   margin-left: 220px;
 }
 
+@media screen and (max-width: 1500px) {
+  .about {
+    margin-left: 60px;
+    margin-right: 120px;
+    margin-top: 100px;
+  }
+}
+
+@media screen and (max-width: 1300px) {
+  .about {
+    margin-left: -30px;
+    margin-right: 120px;
+    margin-top: 100px;
+  }
+}
+
+@media screen and (max-width: 1000px) {
+  .about {
+    margin-left: -80px;
+    margin-right: 120px;
+    margin-top: 100px;
+  }
+}
+
+@media screen and (max-width: 800px) {
+  .about {
+    margin-left: 20px;
+    margin-right: 120px;
+    margin-top: 100px;
+  }
+}
+
 @media screen and (max-width: 600px) {
   .about {
-  margin-left: -40px;
-  margin-right: 120px;
-  margin-top: 100px;
-}
+    margin-left: -40px;
+    margin-right: 120px;
+    margin-top: 100px;
+  }
 }
 </style>
