5ae86d29f1ee3796670e6446f0a4fb9bdac59caf
siggi <siegfried.hamm@gmail.com>
2021-11-28 20:46:09 +0100

    try link in footer

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,7 @@
       </div>
 
       <div class="TitleDiv">
-        <p class="Headline">
-          The Wonderful Travel Blog ©<a
-            href="https://portfolio-siggi-ai.herokuapp.com/works"
-            >back to portfolio</a
-          >
-        </p>
+        <p class="Headline">The Wonderful Travel Blog ©</p>
       </div>
       <div class="menuBar">
         <router-link class="navButtonHome" to="/">Home</router-link>&nbsp;
